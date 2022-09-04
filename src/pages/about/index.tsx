6a36885124d875929edc20559512c6999fc59568
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2022-09-04 22:15:43 +0530

    about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import "./styles.scss"
 import BasicInfo from "../../data/basic_info.json";
-
+import { ReactComponent as InfoIcon } from '../../images/info.svg';
 const AboutPage = () => {
     return (<Row className="about">
-        <Col md={2} className={"d-none d-md-block d-lg-block info-left"}>
-            <p className="fs-4">Who</p>
-            <p className="fs-3">AM</p>
-            <p className="fs-1">I</p>
+        <Col md={2} className={"d-none d-md-block d-lg-block info-left text-center"}>
+            <InfoIcon />
         </Col>
         <Col md={6} >
             <p>{BasicInfo.about.para1}</p>
-
         </Col>
     </Row>)
 }
--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 
+import WorkInfo from "../../data/work_experince.json"
 
 
 const WorkPage = () => {
-    return null
+    return (<Row className="about">
+        <Col md={2} className={"d-none d-md-block d-lg-block info-left"}>
+
+        </Col>
+        <Col md={6} >
+
+
+        </Col>
+    </Row>)
 }
 
 
