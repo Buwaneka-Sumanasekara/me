2b812f3d36ddec698dce38cf6bce060efb028399
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2022-09-04 22:28:20 +0530

    about

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-
+import "./styles.scss"
 import WorkInfo from "../../data/work_experince.json"
-
+import { ReactComponent as WorkIcon } from '../../images/work.svg';
 
 const WorkPage = () => {
     return (<Row className="about">
-        <Col md={2} className={"d-none d-md-block d-lg-block info-left"}>
-
+        <Col md={2} className={"d-none d-md-block d-lg-block info-left text-center"}>
+            <WorkIcon />
         </Col>
         <Col md={6} >
 
