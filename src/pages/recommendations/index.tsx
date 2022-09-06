8980f792144ec0b1b2de11ad16960dd279ce541d
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2022-09-06 19:22:07 +0530

    update

--- src/pages/recommendations/index.tsx
+++ src/pages/recommendations/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Badge, Col, Row } from "react-bootstrap";
+import { Image, Col, Row } from "react-bootstrap";
 import "./styles.scss"
+import RecInfo from "../../data/recommendations.json";
 import BasicInfo from "../../data/basic_info.json";
 import { ReactComponent as PeopleIcon } from '../../images/people.svg';
 const RecommendationPage = () => {
@@ -8,8 +9,22 @@ const RecommendationPage = () => {
         <Col md={2} className={"d-none d-md-block d-lg-block info-left text-center"}>
             <PeopleIcon />
         </Col>
-        <Col md={6} >
-
+        <Col md={6}>
+            <Row>
+                <Col>
+                    <p>Note: Grabbed from my Linkdin account, You can check more details from <a target={"_blank"} href={`${BasicInfo.contacts.linkdin}/details/recommendations/`}>here</a></p>
+                </Col>
+            </Row>
+            {RecInfo.map((item, index) => (
+                <Row className="pb-5 item">
+                    <Col md={2} xs={4}><Image rounded={true} roundedCircle={true} src={item.image} /></Col>
+                    <Col md={10} xs={8}>
+                        <h4>{item.name}</h4>
+                        <p>{item.designation}</p>
+                        <label className="item-des"><i>{`"${item.des}"`}</i></label>
+                    </Col>
+                </Row>
+            ))}
         </Col>
     </Row>)
 }
