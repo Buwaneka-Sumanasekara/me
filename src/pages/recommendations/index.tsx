04d61dfaebfce126136ab07dbbc852c55a2f274b
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2024-04-10 21:37:18 +0530

    update

--- src/pages/recommendations/index.tsx
+++ src/pages/recommendations/index.tsx
@@ -4,6 +4,7 @@ import "./styles.scss"
 import RecInfo from "../../data/recommendations.json";
 import BasicInfo from "../../data/basic_info.json";
 import { ReactComponent as PeopleIcon } from '../../images/people.svg';
+import { ReactComponent as PersonIcon } from '../../images/person.svg';
 const RecommendationPage = () => {
     return (<Row className="page">
         <Col md={2} className={"d-none d-md-block d-lg-block info-left text-center"}>
@@ -12,13 +13,17 @@ const RecommendationPage = () => {
         <Col md={6}>
             <Row>
                 <Col>
-                    <p>Note: Grabbed from my Linkdin account, You can check more details from <a target={"_blank"} href={`${BasicInfo.contacts.linkdin}/details/recommendations/`}>here</a></p>
+                    <p>Note: Grabbed from my Linkdin account, You can check more details from <a target={"_blank"} href={`${BasicInfo.contacts.linkdin}/details/recommendations/`} rel="noreferrer">here</a></p>
                 </Col>
             </Row>
             {RecInfo.map((item, index) => (
                 <Row className="pb-5 item">
-                    <Col md={2} xs={4}><Image rounded={true} roundedCircle={true} src={item.image} /></Col>
-                    <Col md={10} xs={8}>
+                     <Col md={2} className="text-center">
+
+                        <PersonIcon/>
+                    </Col>    
+                    <Col>
+                   
                         <h4>{item.name}</h4>
                         <p>{item.designation}</p>
                         <label className="item-des"><i>{`"${item.des}"`}</i></label>
