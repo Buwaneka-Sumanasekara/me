750dc346e1c508ffc48ba4edae193db34c99c69d
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2024-07-08 19:36:05 +0530

    changed image and responsive issues fixed

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,36 +4,63 @@ import ContactList from "../../components/ContactList";
 import NavBar from "../../components/NavBar";
 import ProfilePic from "../../components/ProfilePic/ProfilePic";
 
-import BasicInfo from '../../data/basic_info.json';
+import BasicInfo from "../../data/basic_info.json";
 
-import "./styles.scss"
+import "./styles.scss";
 
+const HeaderContactInfo = () => {
+  return (
+    <div>
+      <h2>{"Hello"}</h2>
+      <div id={"header-intro-sub"}>
+        <h4>{"I'm "}</h4>
+        <h1>{BasicInfo.displayName}</h1>
+      </div>
+      <ContactList showOnMobile={false} />
+    </div>
+  );
+};
 
 const HeaderInfo = () => {
-    return <div className="">
-        <h2>{"Hello"}</h2>
-        <div id={"header-intro-sub"}><h4>{"I'm "}</h4><h1>{BasicInfo.displayName}</h1></div>
-        <ContactList showOnMobile={false} />
-        <NavBar />
+  return (
+    <div className="container">
+      <Row>
+        <Col className="col-md-10 my-auto">
+          <HeaderContactInfo />
+        </Col>
+        <Col className="col-md-2">
+          <ProfilePic />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <NavBar />
+        </Col>
+      </Row>
     </div>
-}
-
+  );
+};
 
 const Header = () => {
+  return (
+    <Row className="header">
+      {/* on desktop screens */}
+      <Col id={"header-intro-desktop"} className={"col-md-12 pt-4"}>
+        <HeaderInfo />
+      </Col>
 
-    return <Row className="header">
-        {/* on desktop screens */}
-        <Col id={"header-intro-desktop"} className={"col-md-10 pt-5"}>
-             <HeaderInfo />    
-        </Col>
-        <Col id={"header-prof-pic"} className={"col-md-2"}>
-            <ProfilePic />
-        </Col>
-        {/* on mobile tab screens */}
-        <Col sm={12} id={"header-intro-mobile"} className={"text-center mt-3"}>
-            <HeaderInfo />
-        </Col>
-    </Row >
-}
+      {/* on mobile tab screens */}
+      <Col
+        sm={12}
+        id={"header-intro-mobile"}
+        className={"text-center mt-3"}
+      >
+        <HeaderContactInfo/>
+        <ProfilePic />
+        <NavBar />
+      </Col>
+    </Row>
+  );
+};
 
-export default Header
+export default Header;
